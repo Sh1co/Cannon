74fb468f7d1234e2314765a11ba0eb5733387b7f
Sherif Nafee <shico.m.nafee@gmail.com>
2022-02-28 19:32:25 +0300

    Make projectiles destroy targets

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private float _lifeTime = 3.0f;
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.CompareTag("Target"))
+        {
+            Destroy(other.collider.gameObject);
+            Destroy(gameObject);
+        }
+    }
+
+
+    private void Update()
+    {
+        timeCounter += Time.deltaTime;
+        if (timeCounter >= _lifeTime)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private float timeCounter = 0;
+}
